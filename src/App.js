35745f73072f3f2d0b1f4b8ba1d49e70968aacdb
nidhi <nidhi9781@gmail.com>
2023-09-04 20:07:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center font-bold text-green-800 p-10 m-10">
+      <h1>Netflix GPT </h1>
+    </div>
+  );
+}
+
+export default App;
